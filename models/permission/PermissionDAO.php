d4466ff7aabda30c7b6c7ba9a29451803d83fe1b
JagerYe <jager_ye@mail.chungyo.net>
2020-09-29 09:37:47 +0800

    permission model 完成，，SQL新增中文註解

--- models/permission/PermissionDAO.php
+++ models/permission/PermissionDAO.php
@@ -3,131 +3,12 @@ require_once "{$_SERVER['DOCUMENT_ROOT']}/GameConsole/models/permission/Permissi
 require_once "{$_SERVER['DOCUMENT_ROOT']}/GameConsole/models/config.php";
 class PermissionDAO implements PermissionDAO_Interface
 {
-    //新增會員
-    public function insert($account, $password, $name, $email, $phone, $address = null)
-    {
-        try {
-            $dbh = Config::getDBConnect();
-            $dbh->beginTransaction();
-            $sth = $dbh->prepare("INSERT INTO `Members`(`account`, `password`, `name`, `email`, `phone`, `address`, `status`, `creationDate`)
-                VALUES(
-                    (SELECT :account WHERE (SELECT COUNT(`account`) FROM `Members` AS m WHERE `account`=:account) = 0),
-                    :password, :name, :email, :phone, :address, true, NOW()
-            );");
-            $sth->bindParam("account", $account);
-            $password = password_hash($password, PASSWORD_DEFAULT);
-            $sth->bindParam("password", $password);
-            $sth->bindParam("name", $name);
-            $sth->bindParam("email", $email);
-            $sth->bindParam("phone", $phone);
-            $sth->bindParam("address", $address);
-            $sth->execute();
-            $dbh->commit();
-            $sth = null;
-        } catch (PDOException $err) {
-            $dbh->rollBack();
-            $dbh = null;
-            return false;
-        }
-        $dbh = null;
-        return true;
-    }
-
-    //更新會員
-    public function update($id, $name, $email, $phone, $status, $address = null)
-    {
-        try {
-            $dbh = Config::getDBConnect();
-            $dbh->beginTransaction();
-            $sth = $dbh->prepare("UPDATE `Members` SET
-                    `name`=:name,
-                    `email`=:email,
-                    `phone`=:phone,
-                    `address`=:address,
-                    `status`=:status,
-                    `changeDate`=NOW()
-                WHERE `id`=:id;"
-            );
-            $sth->bindParam("id", $id);
-            $sth->bindParam("name", $name);
-            $sth->bindParam("email", $email);
-            $sth->bindParam("phone", $phone);
-            $sth->bindParam("address", $address);
-            $sth->bindParam("status", $status);
-            $sth->execute();
-            $dbh->commit();
-            $sth = null;
-        } catch (PDOException $err) {
-            $dbh->rollBack();
-            $dbh = null;
-            return false;
-        }
-        $dbh = null;
-        return true;
-    }
-
-    //更新密碼
-    public function updatePassword($id, $password)
-    {
-        try {
-            $dbh = Config::getDBConnect();
-            $dbh->beginTransaction();
-            $sth = $dbh->prepare("UPDATE `Members` SET `password`=:password,`changeDate`=NOW()
-                                    WHERE `id`=:id;");
-            $password = password_hash($password, PASSWORD_DEFAULT);
-            $sth->bindParam("id", $id);
-            $sth->bindParam("password", $password);
-            $sth->execute();
-            $dbh->commit();
-            $sth = null;
-        } catch (PDOException $err) {
-            $dbh->rollBack();
-            $dbh = null;
-            return false;
-        }
-        $dbh = null;
-        return true;
-    }
-
-    public function getOneMemberByAccount($account)
-    {
-        try {
-            $dbh = Config::getDBConnect();
-            $sth = $dbh->prepare("SELECT `id`, `account`, `name`, `email`, `phone`, `address`, `status`, `creationDate`, `changeDate` FROM `Members` WHERE `account`=:account;");
-            $sth->bindParam("account", $account);
-            $sth->execute();
-            $request = $sth->fetch(PDO::FETCH_ASSOC);
-            $sth = null;
-        } catch (PDOException $err) {
-            $dbh = null;
-            return null;
-        }
-        $dbh = null;
-        return $request;
-    }
-
-    public function getOneMemberByID($id)
-    {
-        try {
-            $dbh = Config::getDBConnect();
-            $sth = $dbh->prepare("SELECT `id`, `account`, `name`, `email`, `phone`, `address`, `status`, `creationDate`, `changeDate` FROM `Members` WHERE `id`=:id;");
-            $sth->bindParam("id", $id);
-            $sth->execute();
-            $request = $sth->fetch(PDO::FETCH_ASSOC);
-            $sth = null;
-        } catch (PDOException $err) {
-            $dbh = null;
-            return null;
-        }
-        $dbh = null;
-        return $request;
-    }
 
     public function getALL()
     {
         try {
             $dbh = Config::getDBConnect();
-            $sth = $dbh->prepare("SELECT `id`, `account`, `name`, `email`, `phone`, `address`, `status`, `creationDate`, `changeDate` FROM `Members`;");
+            $sth = $dbh->prepare("SELECT * FROM `Permissions`;");
             $sth->execute();
             $request = $sth->fetch(PDO::FETCH_ASSOC);
             $sth = null;
@@ -137,47 +18,6 @@ class PermissionDAO implements PermissionDAO_Interface
         }
         $dbh = null;
         return $request;
-    }
-
-    public function doLogin($account, $password)
-    {
-        try {
-            $dbh = Config::getDBConnect();
-            $sth = $dbh->prepare("SELECT COUNT(`id`) AS `check`, `id` FROM `members` WHERE `account`=:account;");
-            $sth->bindParam("account", $account);
-            $sth->execute();
-            $request = $sth->fetch(PDO::FETCH_ASSOC);
-            if ($request['check'] != 1) {
-                throw new Exception("帳號密碼錯誤");
-            }
-
-            $sth = $dbh->prepare("SELECT `password` FROM `members` WHERE `id`=:id");
-            $sth->bindParam("id", $id);
-            $sth->execute();
-            $request = $sth->fetch(PDO::FETCH_NUM);
-            $sth = null;
-        } catch (Exception $err) {
-            $dbh = null;
-            return false;
-        }
-        $dbh = null;
-        return password_verify($password, $request['0']);
-    }
-
-    public function checkMemberExist($id)
-    {
-        try {
-            $dbh = Config::getDBConnect();
-            $dbh->beginTransaction();
-            $sth = $dbh->prepare("SELECT COUNT(*) FROM `Members` WHERE `account` = :account;");
-            $sth->bindParam("account", $id);
-            $sth->execute();
-            $request = $sth->fetch(PDO::FETCH_NUM);
-        } catch (Exception $err) {
-            $dbh = null;
-            return false;
-        }
-        return $request['0'] > 0;
     }
 
 }
--- moreDate/createDatabaseAndSampleData.sql
+++ moreDate/createDatabaseAndSampleData.sql
@@ -6,69 +6,68 @@ USE `GameConsole`;
 DROP TABLE IF EXISTS `Members`;
 CREATE TABLE `Members`(
     `id` INT NOT NULL AUTO_INCREMENT,
-    `account` VARCHAR(20) NOT NULL,-- 帳號
-    `password` TEXT NOT NULL,-- 密碼
-    `name` VARCHAR(20) NOT NULL,-- 姓名
-    `email` VARCHAR(50) NOT NULL,-- 信箱
-    `phone` VARCHAR(20) NOT NULL,-- 電話
-    `address` TEXT,-- 地址
-    `status` BOOLEAN NOT NULL,-- 停權狀態 True可使用，False不可使用
-    `creationDatetime` DATETIME NOT NULL,-- 創立時間
-    `changeDatetime` DATETIME,-- 更動資料時間
+    `account` VARCHAR(20) NOT NULL,
+    `password` TEXT NOT NULL,
+    `name` VARCHAR(20) NOT NULL,
+    `email` VARCHAR(50) NOT NULL,
+    `phone` VARCHAR(20) NOT NULL,
+    `address` TEXT,
+    `status` BOOLEAN NOT NULL COMMENT '啟用為TRUE，停用為FALSE',
+    `creationDatetime` DATETIME NOT NULL,
+    `changeDatetime` DATETIME,
     PRIMARY KEY(`id`)
-) ENGINE = INNODB DEFAULT CHARSET = utf8;
+) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT '會員';
 
 DROP TABLE IF EXISTS `MemberLoginStatus`;
 CREATE TABLE `MemberLoginStatus`(
     `loginID` INT NOT NULL AUTO_INCREMENT,
     `memberID` INT NOT NULL,
-    `cookieID` TEXT NOT NULL,
-    `keepLoggedIn` BOOLEAN NOT NULL,-- 是否要保持登入
-    `loginDatetime` DATETIME NOT NULL,
-    `usageDatetime` DATETIME,
-    `logoutDatetime` DATETIME,
+    `cookieID` TEXT,
+    `keepLoggedIn` BOOLEAN NOT NULL COMMENT '是否為保持登入',
+    `loginDatetime` DATETIME NOT NULL COMMENT '登入時間',
+    `usageDatetime` DATETIME COMMENT '最後一次使用時間',
+    `logoutDatetime` DATETIME COMMENT '登出時間',
     PRIMARY KEY(`loginID`),
     FOREIGN KEY(`memberID`) REFERENCES `Members`(`id`)
-) ENGINE = INNODB DEFAULT CHARSET = utf8;
+) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT '會員登入紀錄';
 
 DROP TABLE IF EXISTS `Commodities`;
 CREATE TABLE `Commodities`(
     `id` INT NOT NULL AUTO_INCREMENT,
-    `name` TEXT NOT NULL,
+    `name` TEXT NOT NULL COMMENT '商品名稱',
     `price` INT NOT NULL,
-    `quantity` INT NOT NULL,
-    `status` BOOLEAN NOT NULL,
-    `text` TEXT,
+    `quantity` INT NOT NULL COMMENT '商品庫存',
+    `status` BOOLEAN NOT NULL COMMENT '商品上下架狀態',
+    `text` TEXT COMMENT '商品介紹內文',
     `image` BLOB,
     `creationDatetime` DATETIME NOT NULL,
     `changeDatetime` DATETIME,
     PRIMARY KEY(`id`)
-) ENGINE = INNODB DEFAULT CHARSET = utf8;
+) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT '商品';
 
 DROP TABLE IF EXISTS `Orders`;
 CREATE TABLE `Orders`(
     `orderID` INT NOT NULL AUTO_INCREMENT,
     `memberID` INT NOT NULL,
-    `address` TEXT NOT NULL,
+    `address` TEXT NOT NULL COMMENT '當次寄送的地址',
     `creationDatetime` DATETIME NOT NULL,
     `changeDatetime` DATETIME,
     PRIMARY KEY(`orderID`),
     FOREIGN KEY(`memberID`) REFERENCES `Members`(`id`)
-) ENGINE = INNODB DEFAULT CHARSET = utf8;
+) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT '訂單';
 
 DROP TABLE IF EXISTS `OrderDetails`;
 CREATE TABLE `OrderDetails`(
     `orderID` INT NOT NULL,
     `commodityID` INT NOT NULL,
-    `price` INT NOT NULL,
-    `quantity` INT NOT NULL,
+    `price` INT NOT NULL COMMENT '當時價格',
+    `quantity` INT NOT NULL COMMENT '購買數量',
     `creationDatetime` DATETIME NOT NULL,
     `changeDatetime` DATETIME,
     PRIMARY KEY(`orderID`,`commodityID`),
     FOREIGN KEY(`orderID`) REFERENCES `Orders`(`orderID`),
     FOREIGN KEY(`commodityID`) REFERENCES `Commodities`(`id`)
-) ENGINE = INNODB DEFAULT CHARSET = utf8;
-ALTER TABLE `GameConsole`.`OrderDetails` ADD INDEX `orderID` (`orderID`);
+) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT '訂單明細';
 
 DROP TABLE IF EXISTS `Employees`;
 CREATE TABLE `Employees`(
@@ -80,20 +79,20 @@ CREATE TABLE `Employees`(
     `creationDatetime` DATETIME NOT NULL,
     `changeDatetime` DATETIME,
     PRIMARY KEY(`id`)
-) ENGINE = INNODB DEFAULT CHARSET = utf8;
+) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT '員工';
 
 DROP TABLE IF EXISTS `EmployeeLoginStatus`;
 CREATE TABLE `EmployeeLoginStatus`(
     `loginID` INT NOT NULL AUTO_INCREMENT,
     `employeeID` INT NOT NULL,
-    `cookieID` TEXT NOT NULL,
-    `keepLoggedIn` BOOLEAN NOT NULL,
-    `loginDatetime` DATETIME NOT NULL,
-    `usageDatetime` DATETIME,
-    `logoutDatetime` DATETIME,
+    `cookieID` TEXT,
+    `keepLoggedIn` BOOLEAN NOT NULL COMMENT '是否保持登入',
+    `loginDatetime` DATETIME NOT NULL COMMENT '登入時間',
+    `usageDatetime` DATETIME COMMENT '最後使用時間',
+    `logoutDatetime` DATETIME COMMENT '登出時間',
     PRIMARY KEY(`loginID`),
     FOREIGN KEY(`employeeID`) REFERENCES `Employees`(`id`)
-) ENGINE = INNODB DEFAULT CHARSET = utf8;
+) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT '員工登入紀錄';
 
 DROP TABLE IF EXISTS `Permissions`;
 CREATE TABLE `Permissions`(
@@ -102,7 +101,7 @@ CREATE TABLE `Permissions`(
     `creationDatetime` DATETIME NOT NULL,
     `changeDatetime` DATETIME,
     PRIMARY KEY(`id`)
-) ENGINE = INNODB DEFAULT CHARSET = utf8;
+) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT = '權限';
 
 DROP TABLE IF EXISTS `PermissionControl`;
 CREATE TABLE `PermissionControl`(
@@ -112,7 +111,7 @@ CREATE TABLE `PermissionControl`(
     PRIMARY KEY(`employeeID`,`permissionID`),
     FOREIGN KEY(`employeeID`) REFERENCES `Employees`(`id`),
     FOREIGN KEY(`permissionID`) REFERENCES `Permissions`(`id`)
-) ENGINE = INNODB DEFAULT CHARSET = utf8;
+) ENGINE = INNODB DEFAULT CHARSET = utf8 COMMENT '權限及員工對照表';
 
 INSERT INTO `Members`(`account`, `password`, `name`, `email`, `phone`, `address`, `status`, `creationDatetime`) VALUES
 ('m01','123456','a','a@s.s','1234567890','aca',true,NOW()),
